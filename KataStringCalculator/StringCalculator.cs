81f1d94eadef83c7f0ab2307056d5a022625b08e
Marya_Amouri <marya.ammori@gmail.com>
2023-07-04 21:44:17 +0300

    add calculator class

--- KataStringCalculator/StringCalculator.cs
+++ KataStringCalculator/StringCalculator.cs
@@ -8,5 +8,28 @@ namespace KataStringCalculator
 {
     public static class StringCalculator
     {
+         public static int Add(string numbers)
+        {
+            if (string.IsNullOrEmpty(numbers))
+                return 0;
+
+            string[] numberArray = numbers.Split(',');
+
+            int sum = 0;
+            foreach (string num in numberArray)
+            {
+                if (int.TryParse(num, out int parsedNum))
+                {
+                    sum += parsedNum;
+                }
+                else
+                {
+                    continue;
+                }
+                
+            }
+
+            return sum;
+        }
     }
 }
